388110adfb6ce2fbc4422ba84253d5f668e8056d
linchangjian <linchangjian@sf-express.com>
2020-10-29 15:34:04 +0800

    [测试内容] 适配0.59.10版本的jsi

--- android/app/src/main/java/com/reactnativejsitester/MainActivity.java
+++ android/app/src/main/java/com/reactnativejsitester/MainActivity.java
@@ -1,55 +1,15 @@
 package com.reactnativejsitester;
 
-import android.os.Bundle;
-
-import com.aniu.jsilib.NativeFunCallUtils;
-import com.facebook.react.NativeModuleRegistryBuilder;
 import com.facebook.react.ReactActivity;
-import com.facebook.react.ReactInstanceManager;
-import com.facebook.react.bridge.ReactContext;
 
-public class MainActivity extends ReactActivity implements ReactInstanceManager.ReactInstanceEventListener {
+public class MainActivity extends ReactActivity {
 
-  // Used to load the 'native-lib' library on application startup.
-  static {
-    System.loadLibrary("jsi-lib");
-  }
-
-
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  @Override
-  protected String getMainComponentName() {
-    return "ReactNativeJSITester";
-  }
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    System.out.println(NativeFunCallUtils.stringFromJNI());
-  }
-
-  @Override
-  protected void onPause() {
-    super.onPause();
-    getReactInstanceManager().addReactInstanceEventListener(this);
-  }
-
-  @Override
-  protected void onResume() {
-    super.onResume();
-    getReactInstanceManager().addReactInstanceEventListener(this);
-  }
-
-  @Override
-  public void onReactContextInitialized(ReactContext context) {
-    NativeFunCallUtils.installJSI(context.getJavaScriptContextHolder().get());
-  }
-
-  @Override
-  public void onPointerCaptureChanged(boolean hasCapture) {
-
-  }
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "ReactNativeJSITester";
+    }
 }
